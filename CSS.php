799b328b3688a8d2592e05ef3fa825015e88f539
extraJoss7 <duduldul479@gmail.com>
2026-02-13 17:31:54 +0700

    backspace

--- CSS.php
+++ CSS.php
@@ -1,14 +1,1 @@
-<!-- General CSS Files
-    <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="modules/fontawesome/css/all.min.css">
-
-    CSS Libraries
-    <link rel="stylesheet" href="modules/datatables/datatables.min.css">
-    <link rel="stylesheet" href="modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
-
-    Template CSS
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/components.css"> -->
-
     <link rel="stylesheet" href="style.css">
