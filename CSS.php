2221fa5054cbcce0c3950c09c84dd9171978a59c
extraJoss7 <duduldul479@gmail.com>
2026-02-13 17:05:59 +0700

    penyesuaian

--- CSS.php
+++ CSS.php
@@ -1,12 +1,14 @@
-<!-- General CSS Files -->
+<!-- General CSS Files
     <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="modules/fontawesome/css/all.min.css">
 
-    <!-- CSS Libraries -->
+    CSS Libraries
     <link rel="stylesheet" href="modules/datatables/datatables.min.css">
     <link rel="stylesheet" href="modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
 
-    <!-- Template CSS -->
+    Template CSS
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/components.css"> -->
+
+    <link rel="stylesheet" href="style.css">
